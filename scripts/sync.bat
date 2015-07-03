79db9ec5d1ec9a386cdc2ea15c455759f50eebae
Sebatian Loncar <sebastian.loncar@gmail.com>
2015-07-03 04:11:48 +0200

    moved git dir

--- scripts/sync.bat
+++ scripts/sync.bat
@@ -3,6 +3,4 @@ cd ..
 
 tools\fastcopy.exe /auto_close Z:\SharpShell /to="C:\Temp\SharpShell"
 
-c:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe C:\Temp\SharpShell\src\SharpShell.sln
-
 cd C:\Temp\SharpShell\scripts
--- scripts/test.bat
+++
@@ -1,8 +0,0 @@
-cd %~dp0
-cd ..
-
-tools\fastcopy.exe /auto_close Z:\PanelShell /to="C:\Temp\PanelShell"
-
-rem cd C:\Temp\PanelShell\src\Core\bin\Debug
-rem PanelShell.exe
-cd C:\Temp\PanelShell\scripts
