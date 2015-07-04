be30f0d9b12d71374acefba13ddf2fe0f6bf38cb
Sebatian Loncar <sebastian.loncar@gmail.com>
2015-07-04 13:40:13 +0200

    split plugins

---
+++ src/panel/Plugins/Menu.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Gtk;
+using Gdk;
+using System.Collections.Generic;
+
+using abanu.core;
+
+namespace abanu.panel
+{
+
+	public class MenuPlugin : TPlugin
+	{
+
+		private ToggleButton button;
+
+		public MenuPlugin()
+		{
+			widget = button = new ToggleButton();
+			button.Add(new Label("Menu"));
+			button.Clicked += (s, e) => {
+				if (button.Active)
+					Open();
+				else
+					Close();
+			};
+		}
+
+		private LauncherMenuWindow win;
+
+		public void Close()
+		{
+			win.Hide();
+		}
+
+		public void Open()
+		{
+			if (win == null) {
+				win = new LauncherMenuWindow(this);
+			}
+			win.Show();
+			var alloc = button.Allocation;
+			win.Move(alloc.X, alloc.Height);
+		}
+
+		public class LauncherMenuWindow : Gtk.Window
+		{
+
+			private MenuPlugin plug;
+
+			public LauncherMenuWindow(MenuPlugin plug)
+				: base(Gtk.WindowType.Toplevel)
+			{
+				this.plug = plug;
+				SetSizeRequest(500, 500);
+				var wid = new LauncherWidget();
+				Add(wid);
+				wid.Show();
+				Decorated = false;
+			}
+
+			protected override bool OnFocusOutEvent(EventFocus evnt)
+			{
+				Hide();
+				return base.OnFocusOutEvent(evnt);
+			}
+
+			protected override void OnHidden()
+			{
+				plug.button.Active = false;
+				base.OnHidden();
+			}
+
+		}
+
+	}
+
+
+
+
+}
+
---
+++ src/panel/Plugins/PlugLib.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+using Gdk;
+using System.Collections.Generic;
+
+using abanu.core;
+
+namespace abanu.panel
+{
+
+	public class TPlugin
+	{
+
+		public Widget widget;
+
+	}
+
+}
+
