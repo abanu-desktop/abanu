b736ff681e2daa10844a08a558c291a00097ccb3
Arakis <sebastian.loncar@gmail.com>
2015-07-05 16:47:06 +0200

    make script

---
+++ make.cmd
@@ -0,0 +1,5 @@
+rem set AssemblySearchPaths=C:\Temp\dl\GtkSharp\3.0\lib\gtk-sharp-3.0;C:\Temp\dl\abanu\lib;c:\Windows\assembly
+set AssemblySearchPaths=
+
+call kill.cmd
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe src\abanu.sln
---
+++ run.cmd
@@ -0,0 +1,2 @@
+rem set GTK_BASEPATH=C:\Temp\dl\GtkSharp\3.0
+bin\abanu.panel.exe
